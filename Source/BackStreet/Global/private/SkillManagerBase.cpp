c9e8bfe7c1089be4ca9f39bbe18a88f2643ee4c8
kuble <cw6280@gmail.com>
2023-11-10 19:10:42 +0900

    [feat] SkillManagerBase추가
    [feat] SkillManagerBase.h : SkillManagerBase 헤더 추가
           SkillManagerBase.c : SkillManagerBase 정의 추가

---
+++ Source/BackStreet/Global/private/SkillManagerBase.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../../Global/public/BackStreet.h"
+#include "SkillManagerBase.generated.h"
+
+/*
+ */
+UCLASS()
+class BACKSTREET_API USkillManagerBase : public UObject
+{
+	GENERATED_BODY()
+	public:
+	USkillManagerBase();
+
+	UFUNCTION()
+	void InitSkillManagerBase(class ABackStreetGameModeBase* NewGamemodeRef);
+
+	UFUNCTION()
+	void ActivateSkill(FSkillSetStruct SkillSet, AActor* Causer, class ACharacterBase* Target);
+
+	UFUNCTION()
+	void ComposeSkillMap(int32 SkillID, AActor* Causer, ACharacterBase* Target);
+
+	UFUNCTION()
+	class ASkillBase* MakeSkillBase(int32 SkillID);
+
+	UFUNCTION()
+	float GetDelayInterval(int32 SkillListIdx, TArray<float> SkillIntervalList);
+private:
+	UPROPERTY()
+	FSkillSetStruct SkillSetStruct;
+
+	UPROPERTY()
+	TArray<int32> SkillIDArray;
+
+	UPROPERTY()
+	TArray<float> SkillIntervalArray;
+
+	UPROPERTY()
+	TMap<int32, class ASkillBase*> SkillRefMap;
+
+	UPROPERTY()
+	TWeakObjectPtr<class ABackStreetGameModeBase> GamemodeRef;
+};
