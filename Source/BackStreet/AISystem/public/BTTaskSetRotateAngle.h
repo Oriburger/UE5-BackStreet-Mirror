eb122be250c2cefc05aca10edbdb021623dfd376
Jaeho Lee <uss425@naver.com>
2023-12-20 17:41:54 +0900

    [test] AI 스탠딩 패트롤 이주 (~ing) - BTT_SetRotateAngle   - UBTTaskSetRotateAngle   - TurnToTarget을 각도 단위로 진행하기 위한 태스크 노드     - 반드시 TurnToTarget 이전에 호출해야함 - gitignore 수정   - Art 폴더 추가#

--- Source/BackStreet/AISystem/public/BTTaskSetRotateAngle.h
+++ Source/BackStreet/AISystem/public/BTTaskSetRotateAngle.h
@@ -8,7 +8,10 @@
 #include "BTTaskSetRotateAngle.generated.h"
 
 /**
- * 
+  - Name        : UBTTaskSetRotateAngle
+  - Descirption : Specify rotate angle to turn.
+                  After execute this node, you have to execute BTT_TurnToTarget.
+  - Date        : 2023/12/19 LJH
  */
 UCLASS()
 class BACKSTREET_API UBTTaskSetRotateAngle : public UBTTaskNode
@@ -19,10 +22,13 @@ public:
 	UBTTaskSetRotateAngle();
 
 	UPROPERTY(EditInstanceOnly)
-		FBlackboardKeySelector TargetCharacterBBKey;
+		FBlackboardKeySelector ChaseEndFlagBBKey;
 
 	UPROPERTY(EditInstanceOnly)
-		FBlackboardKeySelector TargetLocationBBKey;
+		FBlackboardKeySelector TargetCharacterBBKey;
+	
+	UPROPERTY(EditInstanceOnly)
+		FBlackboardKeySelector TargetRotationBBKey;
 
 protected:
 	//BT에 블랙보드가 설정이 되어있지 않은 경우를 방지
@@ -34,6 +40,17 @@ protected:
 	UFUNCTION()
 		void LogMessage(FString str, FVector2D vec);
 
+	UFUNCTION()
+		bool GetIsPatrolTaskDone();
+
+protected:
+	//L, R
+	UPROPERTY(EditInstanceOnly)
+		float RotateAngleLeft;
+	
+	UPROPERTY(EditInstanceOnly)
+		float RotateAngleRight;
+
 private:
 	//소유자 캐릭터 약참조
 	TWeakObjectPtr<class AEnemyCharacterBase> OwnerCharacterRef;
@@ -41,6 +58,6 @@ private:
 	//블랙보드 컴포넌트
 	TWeakObjectPtr<UBlackboardComponent> BlackboardRef;
 
-	UPROPERTY()
-		FVector TargetLocation;
+	//Flag that means its turn is right or left.	
+	bool bIsRightTurn;
 };
