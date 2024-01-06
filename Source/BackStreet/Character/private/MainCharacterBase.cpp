76021f01e6f3978d7e5d37b366c6f7a42b6fc58a
kuble <cw6280@gmail.com>
2024-01-06 23:47:13 +0900

    [fix] 스킬게이지 충전 안되었을때도 애니메이션 나가는 문제 해결
    - 스킬 게이지 충전 안되었을 때도 애니메이션 나가는 문제 해결

--- Source/BackStreet/Character/private/MainCharacterBase.cpp
+++ Source/BackStreet/Character/private/MainCharacterBase.cpp
@@ -367,7 +367,7 @@ void AMainCharacterBase::TrySkill()
 	if (CharacterState.CharacterActionState == ECharacterActionType::E_Skill
 		|| CharacterState.CharacterActionState != ECharacterActionType::E_Idle) return;
 	 
-	if (GetCurrentWeaponRef()->WeaponID == 0||GetCharacterState().CharacterCurrSkillGauge==0)
+	if (GetCurrentWeaponRef()->WeaponID == 0||GetCharacterState().CharacterCurrSkillGauge<GetCurrentWeaponRef()->GetWeaponStat().SkillGaugeInfo.SkillCommonReq)
 	{
 		GamemodeRef->PrintSystemMessageDelegate.Broadcast(FName(TEXT("스킬을 사용할 수 없습니다. ")), FColor::White);
 		return;
