a6d68e82ba04dfc9e68824878cc068d39a884ea3
Jaeho Lee <uss425@naver.com>
2024-03-03 03:42:27 +0900

    [fix] 보스에게 죽었을 때, 챕터 클리어가 되는 오류 수정 - WaveManager   - 플레이어 사망시, 보스 캐릭터도 사망하게 되어 챕터가 클리어 됨   - AWaveManager::CheckWaveCategoryByType     - 플레이어 유효성 & 잔여 체력 체크로 해결

--- Source/BackStreet/StageSystem/private/WaveManager.cpp
+++ Source/BackStreet/StageSystem/private/WaveManager.cpp
@@ -65,10 +65,19 @@ void AWaveManager::CheckWaveCategoryByType(class AStageData* Target, AEnemyChara
 		}
 	}
 	else if (Target->GetStageTypeInfo().WaveType == EWaveCategoryInfo::E_TimeLimitWave)
-			ManageWaveMonsterCount(Target, Enemy->CharacterID, false);
+	{
+		ManageWaveMonsterCount(Target, Enemy->CharacterID, false);
+	}
 	else if (Target->GetStageCategoryType() == EStageCategoryInfo::E_Boss)
 	{
-		if (Target->GetMonsterList().IsEmpty())
+		float currHP = 0.0f;
+		if (IsValid(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)))
+		{
+			ACharacterBase* playerRef = Cast<ACharacterBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+			currHP = IsValid(playerRef) ? playerRef->GetCharacterState().CharacterCurrHP : 0.0f;
+		}
+		
+		if (Target->GetMonsterList().IsEmpty() && currHP > 0.0f)
 		{
 			Target->DoStageClearTask();
 			Cast<ABackStreetGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()))->UIAnimationDelegate.Broadcast(FName("ChapterClear"));
