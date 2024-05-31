6ac23ab682040328327c72ecf2e4e3f33937668e
kuble <cw6280@gmail.com>
2024-05-31 19:19:53 +0900

    [feat] Crafting Skill Get관련 로직 추가
    -  Crafting Skill Get관련 로직 추가

--- Source/BackStreet/System/CraftingSystem/CraftingManagerBase.cpp
+++ Source/BackStreet/System/CraftingSystem/CraftingManagerBase.cpp
@@ -4,14 +4,18 @@
 #include "CraftingManagerBase.h"
 #include "../../Global/BackStreetGameModeBase.h"
 #include "../../Character/MainCharacter/MainCharacterBase.h"
+#include "../../Item/Weapon/WeaponBase.h"
 #include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 UCraftingManagerBase::UCraftingManagerBase()
 {
 	static ConstructorHelpers::FObjectFinder<UDataTable> craftingRecipeTableFinder(TEXT("/Game/System/CraftingManager/Data/D_CraftingRecipe.D_CraftingRecipe"));
+	static ConstructorHelpers::FObjectFinder<UDataTable> craftingSkillTableFinder(TEXT("/Game/System/CraftingManager/Data/D_CraftingSkillInfo.D_CraftingSkillInfo"));
 	checkf(craftingRecipeTableFinder.Succeeded(), TEXT("CraftingRecipeTable class discovery failed."));
+	checkf(craftingSkillTableFinder.Succeeded(), TEXT("CraftingSkillTable class discovery failed."));
 	CraftingRecipeTable = craftingRecipeTableFinder.Object;
+	CraftingSkillTable = craftingSkillTableFinder.Object;
 }
 
 void UCraftingManagerBase::InitCraftingManager(ABackStreetGameModeBase* NewGamemodeRef)
@@ -91,6 +95,43 @@ TArray<FCraftingRecipeStruct> UCraftingManagerBase::MakeDisplayingRecipeList(EWe
 	displayingRecipeList.Append(unidentifiedRecipeList);
 
 	return displayingRecipeList;
+}
+
+void UCraftingManagerBase::GetSkill(int32 SkillID)
+{
+	AMainCharacterBase* mainCharacter = Cast<AMainCharacterBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if (!IsValid(mainCharacter->GetCurrentWeaponRef())) return;
+	if (mainCharacter->GetCurrentWeaponRef()->GetWeaponStat().WeaponID == 0) return;
+
+	FWeaponStateStruct weaponState = mainCharacter->GetCurrentWeaponRef()->GetWeaponState();
+
+	if (!IsValid(CraftingSkillTable)) { UE_LOG(LogTemp, Log, TEXT("There's no CraftingSkillInfoTable")); return; }
+
+	FString rowName = FString::FromInt(SkillID);
+	FOwnerSkillInfoStruct* skillInfo = CraftingSkillTable->FindRow<FOwnerSkillInfoStruct>(FName(rowName), rowName);
+	if (skillInfo == nullptr) return;
+
+	switch (skillInfo->SkillType)
+	{
+	case ESkillType::E_Weapon0:
+	{
+		weaponState.ActiveWeaponSkillInfo0 = *skillInfo;
+	}
+	case ESkillType::E_Weapon1:
+	{
+		weaponState.ActiveWeaponSkillInfo1 = *skillInfo;
+	}
+	case ESkillType::E_Weapon2:
+	{
+		weaponState.ActiveWeaponSkillInfo2 = *skillInfo;
+	}
+	default:
+		break;
+	}
+
+	mainCharacter->GetCurrentWeaponRef()->SetWeaponState(weaponState);
+	OnGetSkill.Broadcast();
+	return;
 }
 
 ECraftingSlotVisual UCraftingManagerBase::SetRecipeVisual(FCraftingRecipeStruct Recipe)
