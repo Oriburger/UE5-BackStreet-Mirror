bb0b09329959b0e0a57cb9b701a559d033985ee3
kuble <cw6280@gmail.com>
2024-06-01 13:38:04 +0900

    [fix] Save, Instance 비활성화
    - 배열관련 InvalidIndex 크래시로 인해 임시 비활성화

--- Source/BackStreet/System/SaveSystem/BackStreetGameInstance.cpp
+++ Source/BackStreet/System/SaveSystem/BackStreetGameInstance.cpp
@@ -4,7 +4,11 @@
 #include "BackStreetGameInstance.h"
 #include "../../Character/MainCharacter/MainCharacterBase.h"
 
-UBackStreetGameInstance::UBackStreetGameInstance(const FObjectInitializer& ObjectInitializer)
+
+
+
+
+::UBackStreetGameInstance(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	// Constructor implementation
@@ -18,16 +22,18 @@ void UBackStreetGameInstance::Init()
 
 void UBackStreetGameInstance::SaveGameData(FSaveData NewSaveData)
 {
+	/*
 	UBackStreetSaveGame* saveGame = NewObject<UBackStreetSaveGame>();
 	
 	if (saveGame == nullptr) return;
 	saveGame->SaveData = NewSaveData;
 
-	UGameplayStatics::SaveGameToSlot(saveGame, SaveSlotName, 0);
+	UGameplayStatics::SaveGameToSlot(saveGame, SaveSlotName, 0);*/
 }
 
 bool UBackStreetGameInstance::LoadGameSaveData(FSaveData& Result)
 {
+	/*
 	if (UGameplayStatics::DoesSaveGameExist(SaveSlotName, 0))
 	{
 		UBackStreetSaveGame* saveGame = Cast<UBackStreetSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlotName, 0));
@@ -38,6 +44,6 @@ bool UBackStreetGameInstance::LoadGameSaveData(FSaveData& Result)
 		}
 	}
 	UE_LOG(LogTemp, Log, TEXT("Slot is empty"));
-	Result = FSaveData();
+	Result = FSaveData();*/
 	return false;
 }
