ec4b7cccb7a35daa6e6356085871bad9a1d09696
Jaeho Lee <uss425@naver.com>
2024-09-28 19:02:48 +0900

    [fix] 소재 아이콘이 나오지 않는 현상 수정, 크래프팅 UI가 뜨지 않는 현상 수정 등 - 소재 아이콘이 나오지 않는 현상 수정 (+아이템 크래시)   - BP_ItemBase 재생성 - 크래프팅 UI가 뜨지 않는 현상 수정 - 시작화면에서 키입력이 들어가지 않는 현상 수정 - Unknown Error 수정   - DefaultBuildSettings = BuildSettingsVersion.V2로 수정

--- Source/BackStreet.Target.cs
+++ Source/BackStreet.Target.cs
@@ -8,7 +8,7 @@ public class BackStreetTarget : TargetRules
 	public BackStreetTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V3;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
         ExtraModuleNames.AddRange( new string[] { "BackStreet" } );
     }
 }
