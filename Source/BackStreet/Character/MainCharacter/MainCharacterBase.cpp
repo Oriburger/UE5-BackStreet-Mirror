a48ab0a491d90e438a46c8dd4e1d6016b7b8d457
Oriburger <uss425@naver.com>
2024-12-18 18:25:53 +0900

    [feat] 미사용 헤더선언 제거 - CraftBoxBase.h

--- Source/BackStreet/Character/MainCharacter/MainCharacterBase.cpp
+++ Source/BackStreet/Character/MainCharacter/MainCharacterBase.cpp
@@ -8,7 +8,6 @@
 #include "../../Global/BackStreetGameModeBase.h"
 #include "../../System/SaveSystem/BackStreetGameInstance.h"
 #include "../../System/AbilitySystem/AbilityManagerBase.h"
-#include "../../System/CraftingSystem/CraftBoxBase.h"
 #include "../../System/AssetSystem/AssetManagerBase.h"
 #include "../../Item/ItemBase.h"
 #include "../../Item/ItemBoxBase.h"
@@ -231,10 +230,9 @@ void AMainCharacterBase::ZoomIn()
 void AMainCharacterBase::ZoomOut()
 {
 	if (WeaponComponent->GetWeaponStat().WeaponType != EWeaponType::E_Shoot) return;
-
+	
 	SwitchWeapon(false);
 	SetAimingMode(false);
-	
 	//FollowingCamera attach to CameraBoom Component using Interp ===================
 	FLatentActionInfo LatentInfo;
 	LatentInfo.CallbackTarget = this;
